f3f4860c547cace805c98619be884a14ca852772
sneha3128 <80655716+sneha3128@users.noreply.github.com>
2026-09-25 10:42:08 +0530

    Update ProductResource.java

--- src/main/java/com/product/ProductResource.java
+++ src/main/java/com/product/ProductResource.java
@@ -2,4 +2,15 @@ package com.product;
 
 public class ProductResource {
 
+    public void getProducts() {
+        System.out.println("GET /products");
+    }
+
+    public void getProductById(int id) {
+        System.out.println("GET /products/" + id);
+    }
+
+    public void createProduct() {
+        System.out.println("POST /products");
+    }
 }
